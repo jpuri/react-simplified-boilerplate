c6054978621e99a5a2ce0845aab437737c8f2d3f
jpuri <jyotipuri@gmail.com>
2016-04-19 10:08:43 +0530

    chore: add missing eslint dependency

--- js/components/App/index.js
+++ js/components/App/index.js
@@ -3,10 +3,10 @@
 import React from 'react';
 
 // Component with props and validation
-const Comp2 = (props: any) => <div>{ props.children }</div>;
+const App = (props: any) => <div>{ props.children }</div>;
 
-Comp2.propTypes = {
+App.propTypes = {
   children: React.PropTypes.object.isRequired,
 };
 
-export default Comp2;
+export default App;
